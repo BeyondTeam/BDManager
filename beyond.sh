f271f67ef0c83995cc066cd1c33e5b44e2d159ac
ToOfan <toofaneariyayi@gmail.com>
2017-08-05 04:43:39 +0430

    BDManager v1.0

---
+++ beyond.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+install() {
+		sudo apt-get update -y
+		sudo apt-get upgrade -y
+		sudo apt-get install lua5.1 lua-socket lua-sec redis-server curl -y
+		sudo apt-get install libreadline-dev libssl-dev lua5.2 luarocks liblua5.2-dev curl libcurl4-gnutls-dev -y
+		git clone http://github.com/keplerproject/luarocks
+		cd luarocks
+		./configure --lua-version=5.2
+		make build
+		sudo make install
+		sudo luarocks install Lua-cURL -y
+		sudo luarocks install oauth -y
+		sudo luarocks install redis-lua -y
+		sudo luarocks install lua-cjson -y
+		sudo luarocks install ansicolors -y
+		sudo luarocks install serpent -y
+		cd ..
+}
+
+function print_logo() {
+	green "          ____  ____     _____"
+	green "         |  _ )|  _ \   |_   _|___ ____   __  __"
+	green "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
+	green "         |____/|____/     |_|\____/\_____|_/\/\_|"
+	echo -e "\n\e[0m"
+}
+
+function logo_play() {
+    declare -A txtlogo
+    seconds="0.010"
+    txtlogo[1]=" ____  ____     _____"
+    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
+    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
+    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
+    printf "\e[31m\t"
+    for i in ${!txtlogo[@]}; do
+        for x in `seq 0 ${#txtlogo[$i]}`; do
+            printf "${txtlogo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+	echo -e "\e[0m"
+}
+
+function beyondteam() {
+	echo -e "\e[0m"
+	green "     >>>>                       We Are Not Attacker                             "
+	green "     >>>>                       We Are Not Alliance                             "
+	white "     >>>>                       We Are Programmer                               "
+	white "     >>>>                       We Are The Best                                 "
+	red   "     >>>>                       We Are Family                                   "
+	red   "     >>>>                       @BeyondTeam                                     "
+	echo -e "\e[0m"
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+update() {
+	git pull
+}
+
+if [ "$1" = "install" ]; then
+	print_logo
+	beyondteam
+	logo_play
+	install
+elif [ "$1" = "update" ]; then
+	logo_play
+	beyondteam
+	update
+	exit 1
+else
+	print_logo
+	beyondteam
+	logo_play
+	green "Beyond Manager Bot running..."
+	lua ./bot/bot.lua
+fi
